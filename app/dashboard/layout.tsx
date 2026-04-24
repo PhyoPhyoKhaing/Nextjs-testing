f65c7a41b1f138344d0c37e3ca75fe94171bf3e6
PhyoPhyoKhaing <114460250+PhyoPhyoKhaing@users.noreply.github.com>
2026-04-24 11:12:18 +0630

    testingNextjs

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,26 @@
+// import SideNav from "@/app/ui/dashboard/sidenav";
+
+// export default function Layout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+//       <div className="w-full flex-none md:w-64">
+//         <SideNav />
+//       </div>
+//       <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+//     </div>
+//   );
+// }
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/font";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
